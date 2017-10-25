7fcdab4be7f14c7574a447d58b24a928566ac2cf
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-10-24 23:06:41 -0400

    initial code

---
+++ src/deploy-bits-spec.js
@@ -0,0 +1,11 @@
+'use strict'
+
+
+/* eslint-env mocha */
+const deployBits = require('.')
+
+describe('@cypress/deploy-bits', () => {
+  it('write this test', () => {
+    console.assert(deployBits, 'should export something')
+  })
+})
